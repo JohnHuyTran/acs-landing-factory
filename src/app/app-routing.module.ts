e3e654d28a1ac7f4181eb42035427b3a2d9a3944
Giap Nguyen <nguyendinhgiap4997ptit@gmail.com>
2024-04-12 10:32:11 +0700

    add upload form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LandingComponent } from "./pages/landing/landing.component";
+import { ProfilesComponent } from './components/profile/profile.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "landing", pathMatch: "full"},
-  { path: "landing", component: LandingComponent }
+  { path: "landing", component: LandingComponent },
+  { path: "profile", component: ProfilesComponent }
 ];
 
 @NgModule({
